6941d4b13939c85d5af188abebcfdda64ca9bcfd
Amrutha Goje <asdflkj.amrutha@gmail.com>
2026-01-31 14:43:00 +0000

    as i click this redirect me to homepage (_for element <Logo>_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,15 @@
 import { Logo } from '@/components/icons';
+import Link from 'next/link';
 
 export function Header() {
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-      <div className="flex items-center gap-2">
+      <Link href="/" className="flex items-center gap-2 text-foreground no-underline">
         <Logo className="h-6 w-6 text-primary" />
         <h1 className="font-headline text-xl font-semibold tracking-tight">
           CriticalFirst
         </h1>
-      </div>
+      </Link>
     </header>
   );
 }
